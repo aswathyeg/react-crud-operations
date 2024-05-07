43a729acac3d657046c4b90078d0b6ba4981e02c
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-05-07 08:38:51 +0530

    added data

---
+++ src/components/Data.js
@@ -0,0 +1,17 @@
+const employee = [
+  {
+    id: 1,
+    name: "Ash",
+    Age: 18,
+  },
+  {
+    id: 2,
+    name: "John",
+    Age: 19,
+  },
+  {
+    id: 3,
+    name: "Mary",
+    Age: 20,
+  },
+];
