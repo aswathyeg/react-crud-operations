0b5a8efba702a079ec2ddf0b8a227ef3a998de9b
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-05-07 18:46:24 +0530

    added Add component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Add from "./components/Add";
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Add />} />
+          {/* <Route path="/edit" element={<Edit />} /> */}
         </Routes>
       </Router>
     </div>
