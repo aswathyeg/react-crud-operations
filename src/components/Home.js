2456ffbe84837dbaf71fc9c0b167b8c671a0957b
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-05-07 12:48:00 +0530

    added eventhandler

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,49 @@
 import React from "react";
-import Data from "./Data";
+import Employee from "./Data";
+import { Button, Table } from "react-bootstrap";
 const Home = () => {
-  return <div></div>;
+  return (
+    <div style={{ margin: "10rem" }}>
+      <Table striped bordered size="sm">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Employee && Employee.length > 0
+            ? Employee.map((items) => {
+                return (
+                  <tr>
+                    <td>{items.id}</td>
+                    <td>{items.name}</td>
+                    <td>{items.age}</td>
+                    <td>
+                      <Button>Add</Button>
+                      &nbsp;
+                      <Button
+                        onClick={(id) => {
+                          Employee.filter((items) => {
+                            return items.id !== Employee.id;
+                          });
+                        }}
+                      >
+                        Delete
+                      </Button>
+                      &nbsp;
+                      <Button>Edit</Button>
+                      &nbsp;
+                    </td>
+                  </tr>
+                );
+              })
+            : "no data"}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default Home;
