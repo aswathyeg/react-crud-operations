f09df4d0a32d430492754bb559611c32403a8401
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-05-08 12:25:43 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Add from "./components/Add";
+import Edit from "./components/Edit";
 function App() {
   return (
     <div className="App">
@@ -9,7 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<Add />} />
-          {/* <Route path="/edit" element={<Edit />} /> */}
+          <Route path="/edit" element={<Edit />} />
         </Routes>
       </Router>
     </div>
