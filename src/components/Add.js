2e28955b021dba222c916aba0ad74a346dfea346
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-05-08 12:25:58 +0530

    added id

--- src/components/Add.js
+++ src/components/Add.js
@@ -6,6 +6,7 @@ import { v4 as uuid } from "uuid";
 const Add = () => {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
+  const [id, setId] = useState("");
   let history = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault(); //to hide all other events
@@ -13,12 +14,24 @@ const Add = () => {
     let uniqueId = ids.slice(0, 8);
     let a = name;
     let b = age;
-    Employee.push({ id: uniqueId, name: a, age: age });
-    history.push("/create");
+    // let c = id;
+
+    Employee.push({ id: uniqueId, Name: a, Age: b });
+    history("/");
   };
   return (
     <div>
       <Form className="d-grid gap-2" style={{ margin: "15rem" }}>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Control
+            type="text"
+            placeholder="Enter id"
+            required
+            onChange={(e) => {
+              setId(e.target.value);
+            }}
+          ></Form.Control>
+        </Form.Group>
         <Form.Group className="mb-3" controlId="formName">
           <Form.Control
             type="text"
@@ -32,7 +45,7 @@ const Add = () => {
         <Form.Group className="mb-3" controlId="formAge">
           <Form.Control
             type="text"
-            placeholder="Enter Name"
+            placeholder="Enter Age"
             required
             onChange={(e) => {
               setAge(e.target.value);
